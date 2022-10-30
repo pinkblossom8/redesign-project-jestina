7ebec3cb6da0348d16914b6dd194ef92bdf24780
pinkblossom8 <samail0312@hanmail.net>
2022-10-30 15:33:42 +0900

    221030 section-1-bg,hover 작업

--- js/script.js
+++ js/script.js
@@ -20,10 +20,27 @@ function SliderBox1__init() {
     },
 
     autoplay: {
-      delay: 5000, // 5초마다 슬라이드 넘김
+      delay: 4000, // 5초마다 슬라이드 넘김
       disableOnInteraction : false, // 자동 재생 중 건드려도 비활성화되지 않음
     }
   });
 }
 
+// 스크롤 감지 질문!
+
+function ScrollBg__init() {
+  // 스크롤 위치 확인 JS
+  $(window).scroll(function () {
+    let height = $(document).scrollTop();
+    console.log(height);
+
+    // 만약 height가 500 이상이라면 함수 실행해라
+    if (height > 800) {
+      $(".bg-img-1").addClass("active");
+    }
+  });
+}
+
 SliderBox1__init();
+ScrollBg__init();
+AOS.init();
